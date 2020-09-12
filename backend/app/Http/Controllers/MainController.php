be7eaf99010105f501677ca7a313d6a55e42126d
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-09-11 23:57:53 -0300

    first commit

---
+++ backend/app/Http/Controllers/MainController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository;
+use Danganf\MyClass\LogDebug;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    private $pdvApi;
+
+    public function __construct()
+    {
+
+    }
+
+    public function main()
+    {
+        return msgJson([
+            'name' => config('app.name'),
+            'description' => 'project test',
+            'version' => config('app.version'),
+        ]);
+    }
+
+    public function auth( Request $request, UserRepository $userRepository ){
+
+        $result = $userRepository->auth( $request->get('json')->get('login'), $request->get('json')->get('password') );
+        if( !empty( $result ) ){
+            return msgJson( $result );
+        }
+        return msgErroJson( \Lang::get('auth.failed'), 401 );
+
+    }
+}
